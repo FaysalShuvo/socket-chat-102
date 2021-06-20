55ea546c53c03cccd101a8a1abfe73e823c7c38e
FaysalShuvo <shuvonma57@gmail.com>
2021-06-20 19:51:14 +0600

    done

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const app = require("express")();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 
-
-
-app.get("/", (req, res) => {
+app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-http.listen(port, () => {
-  console.log(`port ${port}`);
+io.on("connection", function (socket) {
+  socket.on("user_join", function (data) {
+   
+    this.username = data;
+    socket.broadcast.emit("user_join", data);
+  });
+
+  socket.on("chat_message", function (data) {
+    data.username = this.username;
+    socket.broadcast.emit("chat_message", data);
+  });
+
+  socket.on("disconnect", function (data) {
+    socket.broadcast.emit("user_leave", this.username);
+  });
+});
+
+http.listen(port, function () {
+  console.log("Listening on *:" + port);
 });
